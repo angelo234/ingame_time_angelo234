4663ae43d999be78b7c83ff0c84d1b584a2c42a9
angelo234 <angmatt90@gmail.com>
2020-12-28 11:47:37 -0800

    Initial commit

---
+++ ui/modules/apps/ingame_time_angelo234/app.js
@@ -0,0 +1,110 @@
+angular.module('beamng.apps')
+.directive('ingameTimeAngelo234', ['bngApi', 'StreamsManager', 'UiUnits', function (bngApi, StreamsManager, UiUnits) {
+return {
+templateUrl: 'modules/apps/ingame_time_angelo234/app.html',
+replace: true,
+restrict: 'EA',
+link: function (scope, element, attrs) {
+	
+	// The current overlay screen the user is on (default: null)
+	scope.overlayScreen = null;	
+
+	scope.displayrealtime = false;
+	scope.ampmstyle = false;
+
+	var streamsList = ['electrics'];
+	StreamsManager.add(streamsList);
+
+	var gametime = null;
+
+	function getGameTime(){
+		bngApi.engineLua('core_environment.getTimeOfDay().time', function(data) {
+			//12 am = 0.5
+			//12 pm = 1
+			var time_seconds = data;
+
+			if(time_seconds >= 0.5){
+				time_seconds -= 0.5;
+			}
+			else{
+				time_seconds += 0.5;
+			}
+
+			time_seconds = Math.floor(time_seconds * 86400);
+
+			var hours = Math.floor(time_seconds / 3600);
+
+			var ampmtext = "";
+
+			if(scope.ampmstyle){
+				if(hours < 12){
+					ampmtext = "AM";
+				}
+				else{
+					ampmtext = "PM";
+				}
+
+				hours %= 12;
+
+				if(hours == 0){
+					hours = 12;
+				}
+			}
+	
+	    	var minutes = Math.floor(time_seconds % 3600 / 60);
+
+			gametime = ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2) + " " + ampmtext;
+		});	
+	}
+
+	function getRealTime(){
+		//Code from BeamNG's Simple Time app.js
+
+		var currentDate = new Date(); 
+		var currentHour = currentDate.getHours();
+		var currentMinute = currentDate.getMinutes();
+
+		if (currentMinute < 10) {
+			currentMinute = "0" + currentMinute;
+		}
+
+		var ampmtext = "";
+
+		if(scope.ampmstyle){
+			if(currentHour < 12){
+				ampmtext = "AM"
+			}
+			else{
+				ampmtext = "PM"
+			}
+
+			currentHour %= 12;
+
+			if(currentHour == 0){
+				currentHour = 12;
+			}
+		}
+		
+		return currentHour + ":" + currentMinute + " " + ampmtext;		
+	}
+
+
+	scope.$on('streamsUpdate', function (event, streams) {
+		scope.$evalAsync(function () {
+			if(scope.displayrealtime){
+				scope.time = getRealTime();	
+			}
+			else{
+				getGameTime();
+				scope.time = gametime;
+			}
+		})	
+	});
+
+	// Make sure we clean up after closing the app.
+	scope.$on('$destroy', function () {
+		StreamsManager.remove(streamsList);
+	});	
+},
+};
+}]);
